cf84504b3387e4cfe53f18fc3e1b943b2dca5426
Julia Reis <julyreis.correa@gmail.com>
2025-06-13 22:10:23 -0300

    PAM - Postagens (1)

---
+++ Postagens/Services/PostService.cs
@@ -0,0 +1,35 @@
+﻿using Postagens.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Postagens.Services
+{
+    public class PostService
+    {
+        string base_url = "https://jsonplaceholder.typicode.com";
+        PostModel postagem;
+        JsonSerializerOptions serializerOptions;
+
+        PostService() {
+            serializerOptions = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+
+            };
+        }
+        public async Task<PostModel> getPost()
+        {
+            Uri uri = new Uri($"{base_url}/posts");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage responseMessage = await client.GetAsync(uri);
+            string content = await responseMessage.Content.ReadAsStringAsync();
+            postagem = JsonSerializer.Deserialize < PostModel >(content, serializerOptions);
+            return postagem;
+        }
+    }
+}
